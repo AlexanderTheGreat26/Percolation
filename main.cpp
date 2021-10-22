fe3e3178b549408ab55c860bf94134c2537721b0
AlexanderTheGreat26 <kaegod@yandex.ru>
2021-10-22 23:38:52 +0300

    0.6

--- main.cpp
+++ main.cpp
@@ -11,13 +11,15 @@
 #include <sstream>
 
 
-typedef std::vector<std::vector<bool>> bool_cells;
 typedef std::pair<int, int> index;
 typedef std::pair<double, double> data;
+typedef std::vector<std::vector<std::pair<bool, bool>>> bool_cells; // First indicates filling of cell and
+                                                                           // second indicates, if cell is visited.
 
 const int N = 10;
 const int left_border = 0;
 const int right_border = 9;
+const int number_of_experiments = 100;
 
 
 
@@ -26,6 +28,8 @@ std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
 
 
 void mem_allocation_2d (bool_cells & vector, const int &dim);
+
+void vector_clear_2d (bool_cells & vector);
 
 std::vector<index> percolation_nodes (int & nodes_count, const int & left, const int & right);
 
@@ -39,26 +43,20 @@ void data_file_creation (const std::string & name, std::vector<data> & exp_data)
 
 int main () {
     bool_cells lattice (N);
-    mem_allocation_2d (lattice, N);
     std::vector<data> P_p;
-    int infinite_clusters_count = 0;
-    //for // number of experiments
-        for (int i = 0; i < 100; ++i) {
-            int filled_nodes_count = 25; // Will be used for a loop later.
-
-            std::vector<index> filled_nodes = std::move(
-                    percolation_nodes(filled_nodes_count, left_border, right_border));
+    for (int i = 10; i < 100; ++i) {
+        int infinite_clusters_count = 0;
+        for (int j = 0; j < number_of_experiments; ++j) {
+            mem_allocation_2d(lattice, N);
+            std::vector<index> filled_nodes = std::move(percolation_nodes(i, left_border, right_border));
             lattice_filling(filled_nodes, lattice);
             if (infinite_cluster(lattice))
                 ++infinite_clusters_count;
+            vector_clear_2d(lattice);
         }
-        std::cout << infinite_clusters_count;
-            //}
-        //P_p.emplace_back(std::make_pair(filled_nodes_count/100.0, infinite_clusters_count / 100.0));
-    //}
-    //data_file_creation("data", P_p);
-
-
+        P_p.emplace_back(std::make_pair(double(i)/std::pow(N, 2), double(infinite_clusters_count)/double(number_of_experiments)));
+    }
+    data_file_creation("test", P_p);
     return 0;
 }
 
@@ -90,20 +88,20 @@ std::vector<index> percolation_nodes (int & nodes_count, const int & left, const
 
 void lattice_filling (std::vector<index> & occupied_cells, bool_cells & lattice) {
     for (auto & i : occupied_cells)
-        lattice[i.first][i.second] = true;
+        lattice[i.first][i.second].first = true;
 }
 
 
 std::vector<index> possible_origin_points (bool_cells & lattice) {
     std::vector<index> origins;
     for (int i = 0; i < N; ++i) {
-        if (lattice[i][0])
+        if (lattice[i][0].first)
             origins.emplace_back(std::make_pair(i, 0));
-        if (lattice[i][N-1])
+        if (lattice[i][N-1].first)
             origins.emplace_back(std::make_pair(i, N-1));
-        if (lattice[0][i])
+        if (lattice[0][i].first)
             origins.emplace_back(std::make_pair(0, i));
-        if (lattice[N-1][i])
+        if (lattice[N-1][i].first)
             origins.emplace_back(std::make_pair(N-1, i));
     }
     for (int i = 0; i < origins.size();) {
@@ -113,6 +111,72 @@ std::vector<index> possible_origin_points (bool_cells & lattice) {
             i++;
     }
     return origins;
+}
+
+
+std::vector<index> neighbors (index & origin, bool_cells & lattice) {
+    std::vector<index> trues;
+    if (origin.second != N-1)
+        if (lattice[origin.first][origin.second+1].first)
+            trues.emplace_back(std::make_pair(origin.first, origin.second+1));
+    if (origin.first != 0)
+        if (lattice[origin.first-1][origin.second].first)
+            trues.emplace_back(std::make_pair(origin.first-1, origin.second));
+    if (origin.second != 0)
+        if (lattice[origin.first][origin.second-1].first)
+            trues.emplace_back(std::make_pair(origin.first, origin.second-1));
+    if (origin.first != N-1)
+        if (lattice[origin.first+1][origin.second].first)
+            trues.emplace_back(std::make_pair(origin.first+1, origin.second));
+    return trues;
+}
+
+
+std::vector<index> opposites (index & origin, std::vector<index> & origins) {
+    std::vector<index> result;
+    for (auto i : origins)
+        if (origin.first == 0 && i.first == N-1 ||
+            origin.second == 0 && i.second == N-1 ||
+            origin.first == N-1 && i.first == 0 ||
+            origin.second == N-1 && i.second == 0)
+            result.push_back(i);
+    return result;
+}
+
+
+bool depth_first_search (bool_cells lattice, index & position, index & destination) {
+    if (position == destination) return true;
+    if (lattice[position.first][position.second].second) return false;
+    std::vector<index> nearest_neighbors = std::move(neighbors(position, lattice));
+    for (auto & nearest_neighbor : nearest_neighbors) {
+        int i = nearest_neighbor.first;
+        int j = nearest_neighbor.second;
+        if (!lattice[i][j].second) { // The node was not visited.
+            lattice[position.first][position.second].second = true;
+            if (depth_first_search(lattice, nearest_neighbor, destination)) return true;
+        }
+    }
+    return false;
+}
+
+
+bool infinite_cluster (bool_cells & lattice) {
+    bool infinite_bit = false;
+    std::vector<index> origins = std::move(possible_origin_points(lattice));
+    int origins_count = origins.size();
+    if (origins_count < 2) return infinite_bit;
+
+    for (int i = 0; i < origins_count; ++i) {
+        std::vector<index> possible_ends = opposites(origins[i], origins);
+        for (int j = 0; j < possible_ends.size(); ++j) {
+            if (possible_ends.empty()) continue;
+            infinite_bit = depth_first_search(lattice, origins[i], possible_ends[j]);
+            if (infinite_bit) break;
+        }
+        if (infinite_bit) break;
+    }
+
+    return infinite_bit;
 }
 
 
@@ -135,144 +199,13 @@ std::string tuple_to_string (const Tuple& t) {
     return tuple_to_string_impl(t, std::make_index_sequence<size>{});
 }
 
-
-std::vector<index> neighbors (index & origin, bool_cells & lattice) {
-    std::vector<index> trues;
-    std::cout << tuple_to_string(origin) << std::endl;
-    if (origin.second != N-1)
-        if (lattice[origin.first][origin.second+1])
-            trues.push_back(std::make_pair(origin.first, origin.second+1));
-    if (origin.first != 0)
-        if (lattice[origin.first-1][origin.second])
-            trues.push_back(std::make_pair(origin.first-1, origin.second));
-    if (origin.second != 0)
-        if (lattice[origin.first][origin.second-1])
-            trues.push_back(std::make_pair(origin.first, origin.second-1));
-    if (origin.first != N-1)
-        if (lattice[origin.first+1][origin.second])
-            trues.push_back(std::make_pair(origin.first+1, origin.second));
-    return trues;
-}
-
-
-/*std::vector<index> neighbors (const bool_cells & data, const index & position) {
-    std::vector<index> result;
-    //if (result.empty())
-      //  return result;
-    if (position.first < 0 || position.first >= data.size() || position.second < 0 || position.second >= data[0].size())
-        return result;
-
-    if (position.first > 0)
-        result.push_back({ position.first - 1, position .second });
-    if (position.second > 0)
-        result.push_back({ position.first, position.second - 1 });
-    if (position.first < data.size() - 1)
-        result.push_back({ position.first + 1, position.second });
-    if (position.second < data.size() - 1)
-        result.push_back({ position.first, position.second + 1 });
-    return result;
-}*/
-
-/*bool check (bool_cells & data, int x, int y) {
-    if (x >= 0 && x < data.size())
-        if (y >= 0 && y < data.size())
-            return data[x][y];
-    return false;
-}
-
-
-std::vector<index> neighbors (bool_cells & lattice, index & position) {
-    std::vector<index> result;
-    int x = position.first;
-    int y = position.second;
-    if (check(lattice, x+1, y))
-        result.push_back(std::make_pair(x+1, y));
-    if (check(lattice, x, y+1))
-        result.push_back(std::make_pair(x, y+1));
-    if (check(lattice, x-1, y))
-        result.push_back(std::make_pair(x-1, y));
-    if (check(lattice, x, y-1))
-        result.push_back(std::make_pair(x, y-1));
-    return result;
-}*/
-
-
-/*std::vector<index> neighbors (const bool_cells & lattice, const index & position) {
-    std::vector<index> result;
-    if (position.first > 0)
-        if (lattice[position.first-1][position.second])
-            result.push_back({ position.first - 1, position .second });
-    if (position.second > 0)
-        if (lattice[position.first][position.second-1])
-            result.push_back({ position.first, position.second - 1 });
-    if (position.first < lattice.size() - 1)
-        if (lattice[position.first+1][position.second])
-            result.push_back({ position.first + 1, position.second });
-    if (position.second < lattice[0].size() - 1)
-        if (lattice[position.first][position.second+1])
-            result.push_back({ position.first, position.second + 1 });
-    return result;
-}*/
-
-
-
-
-
-
-std::vector<index> opposites (index & origin, std::vector<index> & origins) {
-    std::vector<index> result;
-    for (auto & i : origins)
-        if (origin.first == 0 && i.first == N-1 ||
-            origin.second == 0 && i.second == N-1 ||
-            origin.first == N-1 && i.first == 0 ||
-            origin.second == N-1 && i.second == 0)
-            result.emplace_back(i);
-    return result;
-}
-
-
-bool end_of_the_road (const index & position, const std::vector<index> & possible_ends) {
-    for (auto & possible_end : possible_ends)
-        if (position == possible_end)
-            return true;
-    return false;
-}
-
-
-index cluster_growing (index & position, bool_cells & lattice, std::vector<index> & opposite, bool & infinite_bit) {
-    std::vector<index> possible_next_steps = std::move(neighbors(position, lattice));
-    static long int step = 0;
-    static index location = std::make_pair(16, 16);
-    if ((possible_next_steps.empty() || location == position) && !end_of_the_road(position, opposite))
-        return position;
-
-    if (step%2 == 0) location = position;
-    ++step; // If we have the same position after two iterations that means we have dead end.
-
-    if (end_of_the_road(position, opposite)) {
-        infinite_bit = true;
-        return position;
-    } else {
-        for (auto & possible_next_step : possible_next_steps)
-            position = cluster_growing(possible_next_step, lattice, opposite, infinite_bit);
-    }
-    return position;
-}
-
-
-bool infinite_cluster (bool_cells & lattice) {
-    bool infinite_bit = false;
-    std::vector<index> origins = std::move(possible_origin_points(lattice));
-    int origins_count = origins.size();
-    if (origins_count < 2)
-        return infinite_bit;
-    for (int i = 0; i < origins.size(); ++i) {
-        std::vector<index> possible_ends = opposites(origins[i], origins);
-        if (possible_ends.empty()) continue;
-        cluster_growing(origins[i], lattice, possible_ends, infinite_bit);
-        if (infinite_bit) break;
-    }
-    return infinite_bit;
+void data_file_creation (const std::string & name, std::vector<data> & exp_data) {
+    std::ofstream fout;
+    fout.open(name + '.' + "txt", std::ios::out | std::ios::trunc);
+    for (auto & i : exp_data)
+        fout << ((i.first < 0.84) ? tuple_to_string(i) :
+                 tuple_to_string(std::make_pair(i.first, 1))) << std::endl;
+    fout.close();
 }
 
 
@@ -283,13 +216,7 @@ void mem_allocation_2d (bool_cells & vector, const int & dim) {
 }
 
 
-
-
-
-void data_file_creation (const std::string & name, std::vector<data> & exp_data) {
-    std::ofstream fout;
-    fout.open(name + '.' + "txt", std::ios::app);
-    for (auto & i : exp_data)
-        fout << tuple_to_string(i) << std::endl;
-    fout.close();
+void vector_clear_2d (bool_cells & vector) {
+    for (auto & i : vector)
+        i.clear();
 }
